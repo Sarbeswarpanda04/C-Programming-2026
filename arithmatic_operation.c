8560af9907745f8375fb65409258b84493856b51
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-01-07 20:01:51 +0530

    Add arithmetic operations demonstration program

---
+++ arithmatic_operation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x = 5;
+    int y = 2;
+    printf("Initial values are x=%d and y=%d", x, y);
+    printf("the addition of %d & %d is %d", x, y, x + y);
+    printf("\nthe subtraction of %d & %d is %d", x, y, x - y);
+    printf("\nthe multiplication of %d & %d is %d", x, y, x * y);
+    printf("\nthe division of %d & %d is %d", x, y, x / y);
+    return 0;
+}
