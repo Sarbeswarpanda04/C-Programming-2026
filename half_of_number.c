83424086ebfa412c2e3b4860e33b774ab7eece57
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-01-08 12:57:56 +0530

    Add program to calculate half of a number

---
+++ half_of_number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    float half;
+    half = n /2;
+    printf("the half of the numer %d is %f",n,half);
+    return 0;
+}
