6543f0781a01a976e6a13aa4ace6be1bb6fff00a
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-01-08 11:14:43 +0530

    Add program to calculate the sum of two numbers

---
+++ sum_of_2_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter First Number:");
+    scanf("%d",&a);
+    printf("Enter Second Number:");
+    scanf("%d",&b);
+    int sum = a + b;
+    printf("The sum of %d and %d is %d", a, b, sum);
+
+    return 0;
+}
+
+
+//Author: Sarbeswar Panda
